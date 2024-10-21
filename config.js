75f4b42d50f66bc3069225c84b5e96ac9f31a703
sanuser999 <sanunufu@gmail.com>
2024-10-21 18:02:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ const configPath = path.join(__dirname, './config.env')
 const databasePath = path.join(__dirname, './database.db')
 if (existsSync(configPath)) require('dotenv').config({ path: configPath })
 const DATABASE_URL =
+const WORK_TYPE = process.env.WORK_TYPE || 'public';
   process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL
 module.exports = {
   VERSION: require('./package.json').version,
